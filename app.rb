71c09da228cf6d8b1880a65939d18f3f7eeecd68
Till Kahlbrock <till@jimdo.com>
2016-11-17 08:56:12 +0100

    deploy

--- app.rb
+++ app.rb
@@ -4,24 +4,26 @@ require 'sinatra'
 require_relative './parser.rb'
 require_relative './fetcher.rb'
 
-before do
-    headers \
-    "Content-Type"   => "application/json"
-end
+class Libro
+    before do
+        headers \
+        "Content-Type"   => "application/json"
+    end
 
-post '/' do
-    username, password = parse_request_params
-    parser = Parser.new()
-    fetcher = Fetcher.new()
+    post '/' do
+        username, password = parse_request_params
+        parser = Parser.new()
+        fetcher = Fetcher.new()
 
-    media_data = fetcher.fetch_media('https://opac.geesthacht-schwarzenbek.de/opax1/user.C', username, password)
-    media_list = parser.media_list(media_data)
+        media_data = fetcher.fetch_media('https://opac.geesthacht-schwarzenbek.de/opax1/user.C', username, password)
+        media_list = parser.media_list(media_data)
 
-    media_list[1..-1].to_json
-end
+        media_list[1..-1].to_json
+    end
 
-def parse_request_params
-    request.body.rewind
-    body = JSON.parse request.body.read
-    return body['username'], body['password']
+    def parse_request_params
+        request.body.rewind
+        body = JSON.parse request.body.read
+        return body['username'], body['password']
+    end
 end
