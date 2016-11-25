1a898cf6b9c009b3861fdaeb182e82ecde0c3679
Till Kahlbrock <till@jimdo.com>
2016-11-25 09:40:08 +0100

    JWT for Auth

--- libro.rb
+++ libro.rb
@@ -1,38 +1,64 @@
 require 'rubygems'
 require 'json'
 require 'sinatra'
+require 'jwt'
+
 require_relative './parser.rb'
 require_relative './fetcher.rb'
 
 class Libro < Sinatra::Base
-    before do
-        headers \
-        "Content-Type"   => "application/json"
+
+    def initialize
+        @auth_secret = ENV['AUTH_SECRET']
+        @hash_algorithm = 'HS256'
+        @parser = Parser.new
+        @fetcher = Fetcher.new
+        super
+    end
+
+    post '/token/create' do
+        set_headers
+        username, password = credentials_from_request
+        payload = {:username => username, :password => password}
+        token = JWT.encode payload, @auth_secret, @hash_algorithm
+        token.to_s
     end
 
     post '/' do
-        username, password = parse_request_params
-        parser = Parser.new()
-        fetcher = Fetcher.new()
+        set_headers
+        token = assert_and_read_token
+        decoded_token = JWT.decode token, @auth_secret, true, {:algorithm => @hash_algorithm }
+        payload = decoded_token[0]
 
-        media_data = fetcher.fetch_media('https://opac.geesthacht-schwarzenbek.de/opax1/user.C', username, password)
-        media_list = parser.media_list(media_data)
+        media_data = @fetcher.fetch_media('https://opac.geesthacht-schwarzenbek.de/opax1/user.C', payload['username'], payload['password'])
+        media_list = @parser.media_list(media_data)
 
         media_list[1..-1].to_json
     end
 
     options "*" do
         response.headers["Allow"] = "POST"
-        response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
+        response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept, Authorization"
         response.headers["Access-Control-Allow-Origin"] = "*"
         200
     end
 
-    def parse_request_params
+    def assert_and_read_token
+        unless request.env['HTTP_AUTHORIZATION'].nil?
+            match = request.env['HTTP_AUTHORIZATION'].match /^Bearer (.+)/
+            return match[1].nil? ? '' : match[1]
+        end
+        raise 'no token found'
+    end
+
+    def set_headers
+        headers "Content-Type" => "application/json"
+        headers "Access-Control-Allow-Origin" => "*"
+    end
+
+    def credentials_from_request
         request.body.rewind
         body = JSON.parse request.body.read
-
-        response.headers["Access-Control-Allow-Origin"] = "*"
         return body['username'], body['password']
     end
 end
